2af004b31146ea8bab86fa6d1294908f641ed86f
Bharath Kumar <1.bharath.k@gmail.com>
2024-11-18 15:18:50 +0530

    Added files for ANg18

---
+++ final/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MovieComponent } from './movie/movie.component';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'movie/:type/:id', component: MovieComponent },
+    { path: '**', component: LoginComponent }
+  ];
+  
